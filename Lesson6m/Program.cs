5a6703d70a347c346fefc5e4a8e6ae9a62d52a42
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2021-04-14 10:30:11 +0300

    initial commit

---
+++ Lesson6m/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson6m
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
